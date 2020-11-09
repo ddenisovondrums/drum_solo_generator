29bc96d18db2e99b55b60c23944b27b79f9a7423
beerfect <beerfect@yandex.ru>
2020-11-09 20:19:07 +0300

    reverse applicature option

--- console_frontend.py
+++ console_frontend.py
@@ -1,5 +1,11 @@
 from backend import *
 
+reverse = {
+    'enabled': False,
+    'R': 'L',
+    'L': 'R'
+}
+
 comment = f'''
 Пьеса "{user_seed}"
 {bars_in_etude} тактов
@@ -8,14 +14,19 @@ comment = f'''
 Примерно {proportion_of_pauses}% пауз
 Примерно {proportion_of_accents}% акцентов
 Максимум ударов одной рукой подряд {maximum_number_of_notes_played_with_one_hand_in_a_row}
-Пьеса начинается с {starting_hand} руки'''
+Пьеса начинается с {starting_hand} руки
+Аппликатура "отзеркалена" {reverse['enabled']}'''
 
 print(comment)
 
 for bar in piece:
+    # каждый такт выводим на новой строке
     print()
+
     for beat in bar:
         print('\'', end='')
+        
+        # вывод нот в формате "о_О"
         for note in beat:
             if note['its_pause']:
                 print('_',end='')
@@ -23,15 +34,29 @@ for bar in piece:
                 print('O',end='')
             else:
                 print('o',end='')
-
         print('(',end='')
+
+        # вывод аппликатуры в формате (RLrl)
         for note in beat:
-            if note['its_pause']:
-                print('_',end='')
-            elif note['its_accent']:
-                print(note['applicature'],end='')
+            
+            # вывод сгенерированной аппликатуры 
+            if not reverse['enabled']:
+                if note['its_pause']:
+                    print('_',end='')
+                elif note['its_accent']:
+                    print(note['applicature'],end='')
+                else:
+                    print(str.lower(note['applicature']),end='')
+
+            # вывод "зеркальной" аппликатуры
             else:
-                print(str.lower(note['applicature']),end='')
+                if note['its_pause']:
+                    print('_',end='')
+                elif note['its_accent']:
+                    print(reverse[note['applicature']],end='')
+                else:
+                    print(str.lower(reverse[note['applicature']]),end='')
+
         print(')',end='')
 
 print()
